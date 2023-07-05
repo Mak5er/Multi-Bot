f01fd1cf96a0e623e9c49c111bcb1514c7f44439
Replit user <>
2023-07-05 13:15:54 +0300

    ADD admin.py

--- handlers/admin.py
+++ handlers/admin.py
@@ -6,6 +6,6 @@ from config import admin_id
 @dp.message_handler(user_id=admin_id, commands=['download_db'])
 async def download_db(message: types.Message):
     await bot.send_chat_action(message.chat.id, "typing")
-    db_file = 'services/jokes.db'
+    db_file = 'services/users.db'
     with open(db_file, 'rb') as file:
         await bot.send_document(message.chat.id, file)
